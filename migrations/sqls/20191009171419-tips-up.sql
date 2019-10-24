e88158068a095de44f1965046fe5c92598a735ee
Parmod Kumar <parmodkumar@spgroup.com.sg>
2019-10-24 14:00:07 +0800

    add dummy data

--- migrations/sqls/20191009171419-tips-up.sql
+++ migrations/sqls/20191009171419-tips-up.sql
@@ -4,3 +4,6 @@ CREATE TABLE tips (
     "item_tips" text,
     PRIMARY KEY ("id")
 );
+
+insert into tips("item_key", "item_tips") values('key1', 'tip1' );
+insert into tips("item_key", "item_tips") values('key2', 'tip2' );
